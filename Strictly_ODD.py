2b1c76715f130c968d0cd6e0cd0be05f9c626242
Viswesh934 <98519767+Viswesh934@users.noreply.github.com>
2022-06-28 19:59:11 +0530

    t=int(input()) l=list(map(int,input().split()))[:t] for i in range(len(l)):     if l[i]%2!=0:         if i%2==0:             print("False")             break else:     print("True")

---
+++ Strictly_ODD.py
@@ -0,0 +1,9 @@
+t=int(input())
+l=list(map(int,input().split()))[:t]
+for i in range(len(l)):
+    if l[i]%2!=0:
+        if i%2==0:
+            print("False")
+            break
+else:
+    print("True")
