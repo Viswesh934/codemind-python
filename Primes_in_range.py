a1200ee9718697cca549174edd4ad609147473a4
Viswesh934 <98519767+Viswesh934@users.noreply.github.com>
2022-06-30 22:06:45 +0530

    def isprime(num):
    	if num==1:
    	    return False
    	for i in range(2,int(num**0.5)+1):
    	    if num%i==0:
    	        return False
    	        break
    	else:
    	    return True
    m=int(input())
    n=int(input())
    c=0
    d=[]
    for i in range(m,n+1):
        if isprime(i):
            c+=1
    print(c)

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,15 +1,19 @@
-def prime(n):
-    if n==1:
-        return False
-    for i in range(2,int(n**0.5)+1):
-        if n%i==0:
-            return False
-    else:
-        return True
-a=int(input())
-b=int(input())
+def isprime(num):
+
+	if num==1:
+	    return False
+	for i in range(2,int(num**0.5)+1):
+	    if num%i==0:
+	        return False
+	        break
+	else:
+	    return True
+m=int(input())
+n=int(input())
 c=0
-for i in range(a,b+1):
-    if prime(i):
+d=[]
+for i in range(m,n+1):
+    if isprime(i):
         c+=1
 print(c)
+    
