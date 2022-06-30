c9d1d010b31158208ad1d9aeb4e3b75d8189891c
Viswesh934 <98519767+Viswesh934@users.noreply.github.com>
2022-06-30 21:40:14 +0530

    def compound_rate(PV, CRate, tp):     TotalAmount = PV * (pow ((1 + CRate / 100), tp))     CInterest = TotalAmount - PV     print('%0.2f'%TotalAmount)
    PV,CRate,tp =map(float,input().split())  
    compound_rate(PV, CRate, tp)

--- Compound_interest.py
+++ Compound_interest.py
@@ -1,3 +1,7 @@
-p,r,t=map(int,input().split())
-d=pow(1+r/100,t)
-print("%0.2f"%(p*d))
+def compound_rate(PV, CRate, tp):  
+    TotalAmount = PV * (pow ((1 + CRate / 100), tp))
+    CInterest = TotalAmount - PV  
+    print('%0.2f'%TotalAmount)
+    
+PV,CRate,tp =map(float,input().split())  
+compound_rate(PV, CRate, tp) 
