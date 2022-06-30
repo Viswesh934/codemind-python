0db7e8a30027de15e5ff997ed42732793a606c71
Viswesh934 <98519767+Viswesh934@users.noreply.github.com>
2022-06-30 22:15:57 +0530

    def isprime(n):     if n==1:         return False     for i in range(2,int(n**0.5)+1):         if n%i==0:             return False             break     else:         return True n=int(input()) c=0 d=[] for i in range(1,n+1):     if isprime(i)==False:         if n%i==0:             c+=1 print(c)

--- Count_of_the_non-prime_divisors_of_a_given_number.py
+++ Count_of_the_non-prime_divisors_of_a_given_number.py
@@ -1,20 +1,17 @@
-def prime(n):
-    c=0
-    for i in range(1,n+1):
+def isprime(n):
+    if n==1:
+        return False
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
+            break
+    else:
+        return True
+n=int(input())
+c=0
+d=[]
+for i in range(1,n+1):
+    if isprime(i)==False:
         if n%i==0:
             c+=1
-    if c==2:
-        return True
-    else:
-        return False
-n=int(input())
-c1=0
-a=[]
-for i in range(1,n+1):
-    if n%i==0:
-        a.append(i)
-for i in a:
-    if not prime(i):
-        c1+=1
-print(c1)
-        
+print(c)
