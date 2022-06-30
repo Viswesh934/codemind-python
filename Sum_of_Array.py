114a1401312fbbb80601b702fe122896a9215aa8
Viswesh934 <98519767+Viswesh934@users.noreply.github.com>
2022-06-30 21:18:46 +0530

    n=int(input()) l=list(map(int,input().split()))[:n] s=0 for i in range(len(l)):     s+=l[i] print(s)

--- Sum_of_Array.py
+++ Sum_of_Array.py
@@ -1,10 +1,6 @@
 n=int(input())
 l=list(map(int,input().split()))[:n]
-k=0
-p=0
+s=0
 for i in range(len(l)):
-    if i%2==0:
-        k+=l[i]
-    else:
-        p+=l[i]
-print(sum(l))
+    s+=l[i]
+print(s)
