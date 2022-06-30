ce0ab92c785185afa05f7bd598c3da94d21bc9c7
Viswesh934 <98519767+Viswesh934@users.noreply.github.com>
2022-06-30 21:17:58 +0530

    n=int(input()) l=list(map(int,input().split()))[:n] s=0 for i in range(len(l)):     if l[i]%2==0:         s+=l[i] print(s)

--- Even_Sum.py
+++ Even_Sum.py
@@ -1,10 +1,7 @@
 n=int(input())
 l=list(map(int,input().split()))[:n]
-k=0
-p=0
+s=0
 for i in range(len(l)):
     if l[i]%2==0:
-        k+=l[i]
-    else:
-        p+=l[i]
-print(k)
+        s+=l[i]
+print(s)
